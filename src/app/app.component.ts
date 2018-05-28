cb3ee8503ca728cf4fa5fb18bbd8c23e38db492b
Merve Özçifçi <merveozcifci7@gmail.com>
2018-05-28 16:14:07 +0300

    Mobile Menu fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  mobileMenuActive: boolean;
+
+  onMobileMenuButton (event) {
+    this.mobileMenuActive = !this.mobileMenuActive;
+    event.preventDefault();
+  }
+
+  hideMobileMenu(event) {
+    this.mobileMenuActive = false;
+    event.preventDefault();
+  }
 }
